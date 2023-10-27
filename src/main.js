7bde6c07a119c5518882d1687c6805e8bc499aa7
YUVAL SHFRIRI <yuval.shafriri@gmail.com>
2023-10-26 20:40:57 -0700

    add input file for changing data json

--- src/main.js
+++ src/main.js
@@ -1,14 +1,38 @@
 //ver 1.0.0
-import Visualization from './visualization.js';
+
 import * as graphAnalysis from './graphAnalysis.js';
-// const width = window.innerWidth;
-// const height = window.outerHeight;
-const dataFileName = './data/data.json';
+ 
 import SimpleNetwork from './SimpleNetwork.js';
 
-const network = new SimpleNetwork('body', dataFileName); // create a new instance of SimpleNetwork
-//
-  
+let network;
+
+function createNewGraph(data) {
+    if (network) {
+         network = null;
+        // Optional: If there are cleanup tasks to be done before creating a new instance, do them here.
+    }
+    network = new SimpleNetwork('body', data);
+}
+
+const dataFileInput = document.getElementById('dataFileInput');
+
+dataFileInput.addEventListener('change', (event) => {
+     
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const newData = JSON.parse(event.target.result);
+            createNewGraph(newData);
+        };
+        reader.readAsText(file);
+    }
+});
+
+ 
+  //createNewGraph(defaultData);
+
+
 // //console.log("main")  
 // graphAnalysis.centrality(dataFileName).then(result => {
 //     console.log("Degree Centrality Results:", result);
